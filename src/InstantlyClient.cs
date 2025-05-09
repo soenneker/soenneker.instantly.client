9d000ecb68b9e5bb8efa4be73f92c039c31958d8
Jake Soenneker <jake@soenneker.com>
2025-05-09 12:13:59 -0500

    namespace

--- src/InstantlyClient.cs
+++ src/InstantlyClient.cs
@@ -2,18 +2,18 @@ using System;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Soenneker.Dtos.HttpClientOptions;
 using Soenneker.Instantly.Client.Abstract;
 using Soenneker.Utils.HttpClientCache.Abstract;
-using Soenneker.Utils.HttpClientCache.Dtos;
 
 namespace Soenneker.Instantly.Client;
 
-///<inheritdoc cref="IInstantlyClient"/>
+/// <inheritdoc cref="IInstantlyClient"/>
 public class InstantlyClient : IInstantlyClient
 {
     private readonly IHttpClientCache _httpClientCache;
 
-    private readonly HttpClientOptions _options = new() { BaseAddress = "https://api.instantly.ai/api/v1/" };
+    private const string _clientId = nameof(InstantlyClient);
 
     public InstantlyClient(IHttpClientCache httpClientCache)
     {
@@ -22,20 +22,21 @@ public class InstantlyClient : IInstantlyClient
 
     public ValueTask<HttpClient> Get(CancellationToken cancellationToken = default)
     {
-        return _httpClientCache.Get(nameof(InstantlyClient), _options, cancellationToken: cancellationToken);
+        return _httpClientCache.Get(_clientId, () => new HttpClientOptions
+        {
+            BaseAddress = "https://api.instantly.ai/api/v1/"
+        }, cancellationToken);
     }
 
     public void Dispose()
     {
         GC.SuppressFinalize(this);
-
-        _httpClientCache.RemoveSync(nameof(InstantlyClient));
+        _httpClientCache.RemoveSync(_clientId);
     }
 
     public ValueTask DisposeAsync()
     {
         GC.SuppressFinalize(this);
-
-        return _httpClientCache.Remove(nameof(InstantlyClient));
+        return _httpClientCache.Remove(_clientId);
     }
 }
